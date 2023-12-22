358c0be134dad052dd0a487f43801efb6b4bd7a3
Ruslan Gusmanov <gusmanovruslan@gmail.com>
2023-12-22 16:47:17 +0300

    Skip constraint check because stairs length is declared greater than or equal 2
    LeetCode constraints:
    * 2 <= cost.length <= 1000
    * 0 <= cost[i] <= 999

--- leetMinCostClimbingStairs.go
+++ leetMinCostClimbingStairs.go
@@ -4,11 +4,9 @@ func minCostClimbingStairs(cost []int) int {
 	n := len(cost)
 	dp := make([]int, n+2)
 	dp[0] = 0
-	var i int
-	for i = 1; i <= 2 && i <= n; i++ {
-		dp[i] = cost[i-1]
-	}
-	for i = 3; i <= n; i++ {
+	dp[1] = cost[0]
+	dp[2] = cost[1]
+	for i := 3; i <= n; i++ {
 		dp[i] = cost[i-1] + min(dp[i-1], dp[i-2])
 	}
 	dp[n+1] = min(dp[n], dp[n-1])
