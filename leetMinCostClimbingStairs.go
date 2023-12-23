3c7841228ccef1a65c41941da17103904a23cd52
Ruslan Gusmanov <gusmanovruslan@gmail.com>
2023-12-23 21:52:40 +0300

    General approach for a Fibonacci-style problem

--- leetMinCostClimbingStairs.go
+++ leetMinCostClimbingStairs.go
@@ -2,13 +2,11 @@ package leetMinCostClimbingStairs
 
 func minCostClimbingStairs(cost []int) int {
 	n := len(cost)
-	dp := make([]int, n+2)
+	dp := make([]int, n+1)
 	dp[0] = 0
 	dp[1] = cost[0]
-	dp[2] = cost[1]
-	for i := 3; i <= n; i++ {
+	for i := 2; i <= n; i++ {
 		dp[i] = cost[i-1] + min(dp[i-1], dp[i-2])
 	}
-	dp[n+1] = min(dp[n], dp[n-1])
-	return dp[n+1]
+	return min(dp[n], dp[n-1])
 }
